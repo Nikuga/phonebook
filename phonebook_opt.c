c1759aa2c0c7987fd71fa7c32b71393f588505e5
AIRS <makinosakuya@hotmail.com>
2016-03-10 23:16:30 +0800

    optimize with hash table

--- phonebook_opt.c
+++ phonebook_opt.c
@@ -1,15 +1,44 @@
+#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
 
 #include "phonebook_opt.h"
 
-/* FILL YOUR OWN IMPLEMENTATION HERE! */
 entry *findName(char lastname[], entry *pHead)
 {
-    /* TODO: implement */
+    unsigned int hash_value = BKDRHash(lastname);
+    entry *pHead_hash = HashTable[hash_value];
+    while (pHead_hash != NULL) {
+        if (strcasecmp(lastname, pHead_hash->lastName) == 0)
+            return pHead_hash;
+        pHead_hash = pHead_hash->pNext;
+    }
     return NULL;
 }
 
 entry *append(char lastName[], entry *e)
 {
-    return NULL;
+    /* allocate memory for the new entry and put lastName */
+    unsigned int hash_value = BKDRHash(lastName);
+    entry *tmp = (entry *) malloc(sizeof(entry));
+    strcpy(tmp->lastName, lastName);
+    if(HashTable[hash_value]==NULL) {
+        HashTable[hash_value] = tmp;
+        HashTable[hash_value]->pNext = NULL;
+    } else {
+        tmp->pNext = HashTable[hash_value];
+        HashTable[hash_value] = tmp;
+    }
+
+    return e;
+}
+
+unsigned int BKDRHash(char *str)
+{
+    unsigned int seed = 31, hash = 0;
+    while (*str) {
+        hash = hash*seed + (*str++);
+    }
+    return (hash & 0x7FFFFFFF) % HASH_TABLE_SIZE;
 }
--- phonebook_opt.h
+++ phonebook_opt.h
@@ -2,12 +2,12 @@
 #define _PHONEBOOK_H
 
 #define MAX_LAST_NAME_SIZE 16
+#define HASH_TABLE_SIZE    1000
 
 /* TODO: After modifying the original version, uncomment the following
  * line to set OPT properly */
-// #define OPT 1
-typedef struct __PHONE_BOOK_ENTRY {
-    char lastName[MAX_LAST_NAME_SIZE];
+#define OPT 1
+typedef struct __ENTRY_DATA {
     char firstName[16];
     char email[16];
     char phone[10];
@@ -17,10 +17,18 @@ typedef struct __PHONE_BOOK_ENTRY {
     char city[16];
     char state[2];
     char zip[5];
+} entry_data;
+
+typedef struct __PHONE_BOOK_ENTRY {
+    char lastName[MAX_LAST_NAME_SIZE];
     struct __PHONE_BOOK_ENTRY *pNext;
+    entry_data *data;
 } entry;
+
+entry *HashTable[HASH_TABLE_SIZE];
 
 entry *findName(char lastname[], entry *pHead);
 entry *append(char lastName[], entry *e);
+unsigned int BKDRHash(char *str);
 
 #endif
